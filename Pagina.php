fab8faeed7d6061d2dc80d90d4d8cc4d18d97774
Roman Cisneros AT mexinet <elcisnedesantajulia@gmail.com>
2013-05-25 11:04:51 -0500

    Primera version con objeto Pagina funcional

--- Pagina.php
+++ Pagina.php
@@ -1,17 +1,78 @@
 <?php
 class Pagina
 {
-    private $head;
-    private $body;
-    private $nice;
+    private $_titulo;
+    private $_cuerpo;
+    private $_hr; //human readable
+    private $_arr_archivos_css=array();
 
-    function __construct($nice=false)
+    function __construct($titulo='',$hr=false)
     {
-        $this->head="<head><title>Titulo</title><head>";
-        $this->body="<body></body>";
-        $this->nice=$nice;
+        $this->_titulo=$titulo;
+        $this->_hr=$hr;
     }
     
-    
+    public function toHtml()
+    {
+        $head=$this->head();
+        $body=$this->body();
+        $html=<<<html
+<!DOCTYPE html>
+<html>
+$head
+$body
+</html>
+html;
+        //TODO aqui hacer arreglos a html antes de enviar
+        if($this->_hr==false)
+        {
+            $html=str_replace("\n",'',$html);
+        }
+        return $html;
+
+    }
+
+    public function agregarBody($cuerpo)
+    {
+        $this->_cuerpo=$cuerpo;
+    }
+
+    public function agregarTitle($titulo)
+    {
+        $this->_titulo=$titulo;
+    }
+
+    public function agregarCss($nombre)
+    {
+        $this->_arr_archivos_css[]=$nombre;
+    }
+
+    private function head()
+    {
+        $titulo=$this->_titulo;
+        $html_css='';
+        foreach($this->_arr_archivos_css as $valor)
+        {
+            $html_css.=<<<html
+<link rel="stylesheet" type="text/css" href="$valor" />
+html;
+        }
+        return <<<html
+<head>
+<title>$titulo</title>
+$html_css
+</head>
+html;
+    }
+
+    private function body()
+    {
+        $cuerpo=$this->_cuerpo;
+        return <<<html
+<body>
+$cuerpo
+</body>
+html;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "Hola mundo";
+require_once "Pagina.php";
+
+$cuerpo=<<<html
+Hola mundo
+html;
+$pagina=new Pagina('Titulo',true);
+$pagina->agregarCss('estilo.css');
+$pagina->agregarBody($cuerpo);
+$html_pagina=$pagina->toHtml();
+echo $html_pagina;
 ?>
